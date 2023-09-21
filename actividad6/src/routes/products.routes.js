d0fb27eb24e32e710172abe7154b349fb6180e1a
Luca Favarel <lucafavarel@gmail.com>
2023-09-21 18:33:36 -0300

    .

--- actividad6/src/routes/products.routes.js
+++ actividad6/src/routes/products.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import { ProductManager } from "../persistence/productManager.js"
+import { productsService } from "../persistence/index.js"
 
-const productsService = new ProductManager("./productos.json")
 
 const router = Router()
 
+
+// ruta get para mostrar todos los productos, o un limite de productos
 router.get("/", async(req, res) => {
     try {
         const products = await productsService.getProduct()
@@ -22,6 +24,7 @@ router.get("/", async(req, res) => {
     }
 })
 
+// ruta get para pedir por id un solo producto
 router.get("/:pid", async(req,res)=>{
     try {
         const id = parseInt(req.params.pid)
@@ -39,6 +42,7 @@ router.get("/:pid", async(req,res)=>{
     }
 })
 
+// ruta post para agregar un producto al json de productos
 router.post("/", async(req,res)=> {
     try {
         const prodInfo = req.body
@@ -56,7 +60,42 @@ router.post("/", async(req,res)=> {
 
 })
 
+// ruta para actualizar un producto
+router.put("/pid", (req,res)=>{
+    try {
+        const id = parseInt(req.params.pid);
+        const newInfo = req.body;
+        const products = productsService.getProduct()
+        const prodIndex = products.findIndex(p=>p.id === id)
+        if (prod>=0) {
+            const newProdActualiced = [...products]
+            newProdActualiced[prodIndex] = newInfo;
+            products = newProdActualiced;
+            
+            res.json({message:"usuario actualizado"})
+        } else {
+            res.json({message:"el producto no existe"})
+        }
+    } catch (error) {
+        res.json({message:error.message})
+    }
+})
 
+
+ //ruta para eliminar un producto
+ router.delete("/:pid", (req,res)=>{
+    const id = parseInt(req.params.pid)
+    const products = productsService.getProduct()
+    const prodIndex = products.findIndex(p => p.id === id)
+
+    if (prodIndex>=0) {
+        const newProds = products.filter(p=>p.id !== id)
+        products = newProds;
+        res.json({message:"producto eliminado"})
+    } else {
+        res.json({message:"no se ha podido eliminar el producto"})
+    }
+ })
 
 
 
